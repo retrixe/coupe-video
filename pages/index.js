a0a11d3a93d63e4c83ee373622cb733b5ea06d54
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2021-09-10 20:10:29 +0530

    Add support info and set to current time buttons.

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,17 @@ import Title from '../imports/title'
 
 import styles from './index.module.scss'
 
+const secondsToString = (totalSeconds) => {
+  const seconds = totalSeconds % 60
+  const totalMinutes = Math.floor(seconds / 60)
+  const minutes = totalMinutes % 60
+  const totalHours = Math.floor(minutes / 60)
+  const secondsString = Math.floor(seconds).toString().padStart(2, '0')
+  const minutesString = minutes.toString().padStart(2, '0')
+  const hoursString = totalHours.toString().padStart(2, '0')
+  return `${hoursString}:${minutesString}:${secondsString}`
+}
+
 // TODO: Titles and HTML5 meta.
 const Index = () => {
   const videoRef = useRef(null)
@@ -34,6 +45,9 @@ const Index = () => {
     setFileInView(originalFile)
   }
 
+  const handleStartTimeToCurrent = () => setStartTime(secondsToString(videoRef.current.currentTime))
+  const handleEndTimeToCurrent = () => setEndTime(secondsToString(videoRef.current.currentTime))
+
   return (
     <>
       <Title title='Coupe-vidéo' description='Cut videos in the browser, no watermark or upload.' url='/' />
@@ -60,6 +74,13 @@ const Index = () => {
             className='u-full-width' placeholder='hh:mm:ss' id='startTimeInput'
           />
         </div>
+        {fileInView && (
+          <>
+            <button onClick={handleStartTimeToCurrent}>Set Start Time To Current</button>
+            <br />
+            <button onClick={handleEndTimeToCurrent}>Set End Time To Current</button>
+          </>
+        )}
         <div>
           <label htmlFor='endTimeInput'>End Time</label>
           <input
@@ -74,7 +95,15 @@ const Index = () => {
         <hr />
         {fileInView
           ? <video ref={videoRef} width={document.body.clientWidth - 64} controls src={fileInView[1]} />
-          : <h2>Drag and drop a video here.</h2>}
+          : (
+            <>
+              <h2>Drag and drop a video here.</h2>
+              <h4>
+                coupe-vidéo requires the latest version of Chrome, Firefox, Safari, Edge or
+                another compatible browser. <i>You may experience errors if using an older browser.</i>
+              </h4>
+            </>
+            )}
       </div>
     </>
   )
